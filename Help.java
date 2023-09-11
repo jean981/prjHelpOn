a3d54a03574d95e57b171a59c58fffc63fa6fae2
Jean <jeanportugalcadastrar@gmail.com>
2023-09-12 00:24:02 +0100

    use a loop to show the options menu

--- Help.java
+++ Help.java
@@ -1,14 +1,22 @@
 public class Help {
     public static void main(String[] args) throws java.io.IOException {
-        char choice;
+        char choice, ignore;
 
-        System.out.println("Help on: ");
-        System.out.println("1. if");
-        System.out.println("2. switch");
-        System.out.print("Choose one: ");
+        do {
+            System.out.println("Help on: ");
+            System.out.println("1. if");
+            System.out.println("2. switch");
+            System.out.println("3. for");
+            System.out.println("4. while");
+            System.out.println("5. do-while\n");
+            System.out.print("Choose one: ");
 
-        choice = (char) System.in.read();
-        System.out.println();
+            choice = (char) System.in.read();
+
+            do {
+                ignore = (char) System.in.read();
+            } while (ignore != '\n');
+        }while (choice < '1' | choice > '5');
 
         switch (choice){
             case '1':
@@ -23,7 +31,21 @@ public class Help {
                 System.out.println("statement sequence");
                 System.out.println("break;");
                 System.out.println("// ...");
-
+            case '3':
+                System.out.println("The for: \n");
+                System.out.println("for (init; condition; iteration)");
+                System.out.println("statement;");
+                break;
+            case '4':
+                System.out.println("The while: \n");
+                System.out.println("while (condition) statement;");
+                break;
+            case '5':
+                System.out.println("The do-while: \n");
+                System.out.println("do {");
+                System.out.println("    statement;");
+                System.out.println("} while (condition);");
+                break;
             default:
                 System.out.println("Selection not found.");
 
